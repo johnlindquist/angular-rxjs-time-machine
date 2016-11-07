8f1a80c5285fb00e3599c4a6b8676c42150bac5c
John Lindquist <johnlindquist@gmail.com>
2016-11-07 10:45:15 -0700

    formatting

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,5 +1,4 @@
-
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 @Component({
     selector: 'app',
     template: `<h1 class="code">Hello Angular RXJS Time Machine</h1>`
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { App } from './app.component';
+import { App } from './app.component'
 
 
 @NgModule({
